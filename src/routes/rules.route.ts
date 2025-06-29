5e7d5d9ed397b144ca8495b8ed6637e62f9b9347
sahilcateina <sahil@cateina.com>
2025-06-29 13:05:15 +0530

    Initial commit

--- src/routes/rules.route.ts
+++ src/routes/rules.route.ts
@@ -13,22 +13,3 @@ router.post('/apply',controller.applyRules);
 
 export default router;
 
-
-// import express from 'express';
-// import {
-//   updateRule,
-//   deleteRule,
-//   getAllRules,
-//   applyRules,
-//   parseAndCreateRule
-// } from '../controllers/rules.controller';
-
-// const router = express.Router();
-
-// router.put('/:id', updateRule);
-// router.delete('/:id', deleteRule);
-// router.get('/', getAllRules);
-// router.post('/apply',applyRules);
-// router.post('/create', parseAndCreateRule);
-
-// export default router;
